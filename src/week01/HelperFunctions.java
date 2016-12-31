119e8ee41e1a7ab60d2d0d902e400a00e7b7be96
purukaushik <purukaushik@asu.edu>
2016-12-31 12:24:08 -0700

    Added fibanocci

--- src/week01/HelperFunctions.java
+++ src/week01/HelperFunctions.java
@@ -8,11 +8,39 @@ import java.util.function.Function;
  */
 public class HelperFunctions {
 
-  public static void main(String[] args) {
-    // Borrowed with thanks from http://stackoverflow.com/a/22953285
+  static Function<Integer, Integer> factorialFunction() {
     BiFunction<BiFunction, Integer, Integer> helper =
         (f, x) -> (x == 0) ? 1 : x * (Integer) f.apply(f, x - 1);
     Function<Integer, Integer> factorial = x -> helper.apply(helper, x);
-    System.out.println(factorial.apply(5));
+    return factorial;
+  }
+
+
+  @FunctionalInterface
+  interface RecursiveTriFunction<F, A, B, C, R> {
+
+    public R apply(F f, A a, B b, C c);
+  }
+
+  static Function<Integer, Integer> fibanocciFunction() {
+    RecursiveTriFunction<RecursiveTriFunction, Integer, Integer, Integer, Integer> helper = (f, a, b, c) -> {
+      switch (a) {
+        case 0:
+          return b != null ? b : 0;
+        case 1:
+          return c != null ? c : 1;
+        default:
+          int t = c != null && b != null ? (c + b) : 1;
+          return (Integer) f.apply(f, a - 1, c != null ? c : 1, t);
+      }
+    };
+    return (x -> helper.apply(helper, x, null, null));
+  }
+
+  public static void main(String[] args) {
+    // Borrowed with thanks from http://stackoverflow.com/a/22953285
+
+    //System.out.println(factorialFunction().apply(5));
+    System.out.println(fibanocciFunction().apply(9));
   }
 }
